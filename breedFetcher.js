af685809f15317fccaf0575fd9e21dcc7698d182
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-20 10:12:14 -0500

    Remove node modules folder

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
 const request = require("request");
+const query = process.argv.slice(2)[0];
+const search = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
+
+request(search, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  if (data[0]) {
+    console.log(data[0].description);
+  } else {
+    console.log(`Error ${query} cannot be found`);
+  }
+});
